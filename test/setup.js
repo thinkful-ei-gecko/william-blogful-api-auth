5e58d1071bbfefd7e685994432e4f2d3076afba9
William Bae <wbae912@gmail.com>
2019-10-10 22:17:17 -0700

    JWT functionality and tests added

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,10 @@
-process.env.TZ = 'UCT';
+process.env.TZ = 'UTC';
 process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test-jwt-secret';
 
 require('dotenv').config();
 
-process.env.TEST_DB_URL = process.env.TEST_DB_URL
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
   || "postgresql://dunder_mifflin@localhost/blogful-auth-test"
 
 const { expect } = require('chai');
